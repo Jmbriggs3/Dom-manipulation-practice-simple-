a1619c70e6312d260a4632d7a7fdad39fba64ba9
Jmbriggs3 <46725493+Jmbriggs3@users.noreply.github.com>
2020-03-09 22:13:15 -0400

    Add files via upload
    This is an updated version of the DOM practice creating a new separate div with different properties then subsequently appending it to the initially created container ID.

--- myScript.js
+++ myScript.js
@@ -1,7 +1,65 @@
 const container = document.querySelector('#container'); 
 
-const content = document.createElement('div'); 
+content = document.createElement('div'); 
 content.classList.add('content'); 
-content.textContent = 'This is the glorious text-content'; 
+content.textContent = 'This is the glorious text-content';
 
 container.appendChild(content); 
+
+paragraph = document.createElement('p'); 
+paragraph.style.color = 'red'; 
+paragraph.textContent = 'Hey I am red'; 
+
+container.appendChild(paragraph); 
+
+header = document.createElement('h3');
+header.style.color = 'blue'; 
+header.textContent = 'I am a blue h3'; 
+
+container.appendChild(header);  
+
+myDiv = document.createElement('div');  
+myDiv.classList.add('new');
+myDiv.style.border = 'thick solid #0000FF'; 
+myDiv.style.borderColor = 'black'; 
+myDiv.style.backgroundColor = 'pink';
+h1 = document.createElement('h1');
+h1.textContent = 'I am in a div'; 
+myDiv.appendChild(h1); 
+p = document.createElement('p'); 
+p.textContent = 'Me too baby!!'; 
+myDiv.appendChild(p);  
+
+container.appendChild(myDiv);
+
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
